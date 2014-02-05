f4579dd2e9962dc7656e07b2445cc383db0de438
Jason Heron <JasonAHeron@Gmail.com>
2014-02-05 00:16:03 -0800

    Cleaned up fuzzy matching and fusion tool in general. It's ugly but it works well

--- data-fusion/fusion.py
+++ data-fusion/fusion.py
@@ -1,17 +1,28 @@
 __author__ = 'jason'
 import csv
-from Tkinter import Tk
-from tkFileDialog import askopenfilename
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import getopt
+import sys
 
 DEBUG = False
+HEADER_SEARCH = True
+FUZZY_MATCHING = True
 
+def main(argv):
+    try:
+        optlist, args = getopt.getopt(argv, 'i:m::o:f:')
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+        sys.exit(2)
+    arguments = map(list, zip(*optlist))[1]
+    mergeCSV(arguments[0], arguments[1], arguments[2], arguments[3])
+
+def usage():
+    print "\nUsage of fusion.py\nfusion.py -i input.csv -m filetomerge.csv -o outputfile.csv -f newfieldname"
 
 def mergeCSV(c1, c2, outfile, field_name):
-
-    headerSearch = True
-    fuzzy_matching = False
 
     csvfile1 = file(str(c1), 'r')
     csvfile2 = file(str(c2), 'r')
@@ -21,65 +32,46 @@ def mergeCSV(c1, c2, outfile, field_name):
     output = csv.writer(new_csvfile)
     csv2_rows = [row for row in csv2]
 
-    if headerSearch:
+    if HEADER_SEARCH:
         print "headers in " + str(c2) + " " + str(zip(csv2_rows[0], range(0, len(csv2_rows[0]))))
         csv2_extract = int(raw_input("text above formatted ('data','index') select a header index to merge: "))
 
     first_pass = True
     for csv1_row in csv1:
-
         match = False
-        second_match = False
-
         for csv2_row in csv2_rows:
             output_row = csv1_row
             if first_pass:
                 output_row = output_row + [str(field_name)]
-            #found a match
-            if str(csv1_row[0]) == str(csv2_row[0]):
+            elif str(csv1_row[0]) == str(csv2_row[0]):
                 match = True
                 output_row.append(((csv2_row[csv2_extract].split('.'))[0]).replace(',', ''))
-
-        if not match and fuzzy_matching:
+        if (not first_pass) and (not match) and (FUZZY_MATCHING):
+            output_rows = []
+            output_names = []
+            fuzzy_match = False
             for csv2_row in csv2_rows:
-                #didn't find a match but want to try fuzzy matching
-                #found a fuzzy match
-                if (not match) and ((fuzz.partial_ratio(str(csv1_row[0]), str(csv2_row[0])) > 95) or (fuzz.partial_ratio(str(csv2_row[0]), str(csv1_row[0])) > 95)):
-                    #it's the first fuzzy match, ask the usr about it
-                    if (not second_match) and (str(raw_input("Fuzzy match FOR: {" + str(csv1_row[0]) + "} FOUND: {" + str(csv2_row[0]) + "} (y/n):\n")) is "y" or "Y"):
-                        output_row.append(((csv2_row[csv2_extract].split('.'))[0]).replace(',', ''))
-                        match = True
-                        second_match = True
-                    #it's a fuzzy match but it's not the first
-                    elif second_match:
-                        if str(raw_input("WARNING: Second fuzzy match FOR: {" + str(csv1_row[0]) + "} FOUND: {" + str(csv2_row[0]) + "}:\n" + "This will cause an extra col in your data and is probably not what you want (y/n):\n")) is "y" or "Y":
-                            output_row.append(((csv2_row[csv2_extract].split('.'))[0]).replace(',', ''))
-                            
-
-        if not match:
+                output_row = []
+                output_row.extend(csv1_row)
+                if (fuzz.partial_ratio(str(csv1_row[0]), str(csv2_row[0])) > 95) or (fuzz.partial_ratio(str(csv2_row[0]), str(csv1_row[0])) > 95):
+                    output_row.append(((csv2_row[csv2_extract].split('.'))[0]).replace(',', ''))
+                    output_rows.append(output_row)
+                    output_names.append(csv2_row[0])
+                    fuzzy_match = True
+            if fuzzy_match:
+                print "Found fuzzy matches for {" + str(csv1_row[0]) + "}:"
+                for i, name in enumerate(output_names):
+                    print " (" + str(i) + ") " + name
+                extraction_val = str(raw_input("Please select an index or enter 'n' for none: "))
+                if extraction_val is "n":
+                    output_row = csv1_row
+                else:
+                    match = True
+                    output_row = output_rows[int(extraction_val)]
+        if (not match) and (not first_pass):
             print "failed to find " + csv1_row[0] + " in second csv file"
         output.writerow(output_row)
         first_pass = False
 
-
-arg1 = raw_input("Enter name of file to merge data into (press 'c' for file chooser): ")
-if arg1 == "-":
-    arg1 = "GDP_MERGE_ENERGY_PER.csv"
-if arg1 == "c":
-    Tk().withdraw()
-    arg1 = askopenfilename()
-arg2 = raw_input("Enter name of file to merge data from: ")
-if arg2 == "-":
-    arg2 = "POPULATION_DATA_THOUSANDS.csv"
-if arg2 == "c":
-    Tk().withdraw()
-    arg2 = askopenfilename()
-arg3 = raw_input("Enter name of desired output file: ")
-if arg3 == "-":
-    arg3 = "out.csv"
-if arg3 == "c":
-    Tk().withdraw()
-    arg3 = askopenfilename()
-arg4 = raw_input("Enter name of new data field: ")
-
-mergeCSV(arg1, arg2, arg3, arg4)
+if __name__ == "__main__":
+    main(sys.argv[1:])
